cbcb283e26d7c9dc1a6fecfdefe88b8f69ded5b5
Robin Gerber <rob99gerber@gmail.com>
2023-10-26 18:54:44 +0200

    SQL query methods cleaning task

--- backend/services/cleaningPlanService.js
+++ backend/services/cleaningPlanService.js
@@ -9,6 +9,33 @@ const getAllCleaningTasks = async () => {
     }
 }
 
+const createNewCleaningTask = async () => {
+    try{
+        const result = await pool.query('INSERT * INTO claeningtasks')
+    } catch(err) {
+        throw err;
+    }
+}
+
+const deleteCleaningTask = async (taskId) => {
+    try {
+        const result = await pool.query('DELETE FROM cleaningtasks WHERE task_id = $1', [TaskId]);
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
+
+// Example usage:
+try {
+    const taskIdToDelete = 123; // Replace with the actual task ID you want to delete.
+    const deletedTask = await deleteCleaningTask(taskIdToDelete);
+    console.log('Cleaning Task deleted:', deletedTask);
+} catch (err) {
+    console.error('Error deleting Cleaning Task:', err);
+}
+
+
 module.exports = {
     getAllCleaningTasks
 }
